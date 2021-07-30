8fabca4ff1ef67c1ee6940c5dd9c32b87a066ec3
James Dong <jamesjd1024@gmail.com>
2021-07-30 09:12:12 -0500

    Began implementation of Navbar for website

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
+import { BrowserRouter, Route, Link } from 'react-router-dom'; 
+import Resume from './Resume.js'; 
+import Projects from './Projects.js'; 
+import Contact from './Contact.js';
+import About from './About.js'; 
+import React from "react"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="navigation">
+          <div className="navigation-sub">
+            <a href="" className="item">Projects </a>
+            <a href="" className="item">Resume</a>
+            <a href="" className="item">About</a>
+            <a href="" className="item">Contact</a>
+          </div>
+        </div>
     </div>
   );
 }
