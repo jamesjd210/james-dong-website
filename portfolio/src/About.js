5f2796bd68decb9a99b6eb564a42173c742aceed
James Dong <jamesjd1024@gmail.com>
2021-08-02 22:24:36 -0500

    added section to about page

--- portfolio/src/About.js
+++ portfolio/src/About.js
@@ -4,6 +4,17 @@ function About(props) {
   return (
     <div>
       <h1>About</h1>
+      <p className = "About-Paragraph">
+        Hello, my name is James Dong and I am a rising junior Computer Science
+        student at the University of Texas at Austin. My interest computer science
+        mostly developed when I was working in a microbiology lab and I was writing
+        scripts to analyze DNA sequences. From this experience I realized that 
+        computer science was an open ended medium that kept many opportunities
+        in different fields of study open to me. I would not be 100% confined to
+        a single career path. Fortunately for me, I have been given the opportunity
+        to continue my studies at the University of Texas at Austin and I am able
+        to pursue my passions.
+      </p>
     </div>
   )
 }
